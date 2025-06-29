0425ba1b2cde3f7aaee95d2fcda1d2edc6828c74
chocobulldozer <maple7868@naver.com>
2025-06-29 19:35:38 +0900

    local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,4 +1,4 @@
 from .base import * # base.py의 모든 설정을 가져옵니다.
 
-DEBUG = True # 로컬 개발 환경에서는 항상 True여야 합니다.
+DEBUG = False # 로컬 개발 환경에서는 항상 True여야 합니다.
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] # 로컬 개발용 허용 호스트
